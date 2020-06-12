03fa43179010361eada0c84345100d4726e3d5a7
Pham Van Manh <manh.pham21@pn.com.vn>
2020-06-12 14:31:20 +0700

    add index.html file

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-abc
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>hello manh</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1> Manh Master </h1>
+
+</body>
+</html>
